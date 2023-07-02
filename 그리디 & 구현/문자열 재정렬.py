f4052c13919b614f04e68a4b5de1e646e3f7283a
YeoSong <duthd3@naver.com>
2023-07-02 15:49:53 +0900

    Update 문자열 재정렬.py

--- 그리디 & 구현/문자열 재정렬.py
+++ 그리디 & 구현/문자열 재정렬.py
@@ -1,1 +1,16 @@
+data = input()
+new_str = []
+num = 0
+
+for i in data:
+    if 65 <= ord(i) and ord(i) <= 90:
+        new_str.append(i)
+    else:
+       num += int(i)     
+new_str.sort()
+if num != 0:
+    new_str.append(str(num))
+
+print(''.join(new_str))
+
 
